05e60542c7763b88dbdbce008e324f29425226b7
Luiz Gustavo do Valle <luizgvv@gmail.com>
2025-01-17 20:29:48 -0300

    Update LED_1.c
    Adicionando função de inicialização para o LED 1 (que no momento está sendo no pino 13), e a função de inicialização do teclado

--- LED_1.c
+++ LED_1.c
@@ -7,3 +7,17 @@
 // Definições dos pinos do teclado (simplificado para a tecla 1)
 #define L1 1
 #define C1 5
+
+// Inicializa o LED 1
+void init_led() {
+    gpio_init(LED_1);
+    gpio_set_dir(LED_1, GPIO_OUT);
+    gpio_put(LED_1, 0);  // Inicialmente apagado
+}
+
+// Inicializa o teclado
+void init_teclado() {
+    gpio_init(L1); gpio_set_dir(L1, GPIO_OUT); gpio_put(L1, 0);
+    gpio_init(C1); gpio_set_dir(C1, GPIO_IN); gpio_pull_down(C1);
+}
+
