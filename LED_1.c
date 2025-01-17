1ae04303a386e1e7cb6155c93cf5585b482ff1ae
Luiz Gustavo do Valle <luizgvv@gmail.com>
2025-01-17 20:24:32 -0300

    Update LED_1.c
    Definindo as bibliotecas e pinos para o LED e teclado

--- LED_1.c
+++ LED_1.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Definição do pino do LED 1
 #define LED_1 13
+
+// Definições dos pinos do teclado (simplificado para a tecla 1)
+#define L1 1
+#define C1 5
