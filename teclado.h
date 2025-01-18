446b987e9a763a30fe6929df873cc9fd8aaf8a77
pauloh-fm <phfarmar@gmail.com>
2025-01-18 11:26:19 -0300

    feat: criando as primeiras definições para o teclado matricial

---
+++ teclado.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stdio.h>
+// Definições dos pinos usados para linhas e colunas do teclado matricial
+#define LINHA_1 2
+#define LINHA_2 3
+#define LINHA_3 4
+#define LINHA_4 5
+
+#define COLUNA_1 6
+#define COLUNA_2 7
+#define COLUNA_3 8
+#define COLUNA_4 9
+
+// Protótipo das funções
+// void teclado_init(void);
+// uint8_t 
