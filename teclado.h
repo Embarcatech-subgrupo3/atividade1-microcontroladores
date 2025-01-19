6ddbe0dc2bf159a8299b9fa1f2dd724e2d87f44b
pauloh-fm <phfarmar@gmail.com>
2025-01-19 10:45:50 -0300

    feat: create teclas map and init function

--- teclado.h
+++ teclado.h
@@ -1,16 +1,25 @@
-#include <stdint.h>
 #include <stdio.h>
-// Definições dos pinos usados para linhas e colunas do teclado matricial
-#define LINHA_1 2
-#define LINHA_2 3
-#define LINHA_3 4
-#define LINHA_4 5
+#include "pico/stdlib.h"
 
-#define COLUNA_1 6
-#define COLUNA_2 7
-#define COLUNA_3 8
-#define COLUNA_4 9
+// Mapeamento das teclas em uma matriz 4x4
+char teclas[4][4] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
+};
 
-// Protótipo das funções
-// void teclado_init(void);
-// uint8_t 
+// Inicializa o teclado matricial
+void init_teclado() {
+    gpio_init(L1); gpio_set_dir(L1, GPIO_OUT); gpio_put(L1, 0);
+    gpio_init(L2); gpio_set_dir(L2, GPIO_OUT); gpio_put(L2, 0);
+    gpio_init(L3); gpio_set_dir(L3, GPIO_OUT); gpio_put(L3, 0);
+    gpio_init(L4); gpio_set_dir(L4, GPIO_OUT); gpio_put(L4, 0);
+
+    gpio_init(C1); gpio_set_dir(C1, GPIO_IN); gpio_pull_down(C1);
+    gpio_init(C2); gpio_set_dir(C2, GPIO_IN); gpio_pull_down(C2);
+    gpio_init(C3); gpio_set_dir(C3, GPIO_IN); gpio_pull_down(C3);
+    gpio_init(C4); gpio_set_dir(C4, GPIO_IN); gpio_pull_down(C4);
+}
+
+
